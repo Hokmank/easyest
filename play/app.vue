d4f4961a259f657b45801302db948558f3fbd23e
hokman <435999329@qq.com>
2023-10-31 10:46:52 +0800

    update

--- play/app.vue
+++ play/app.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <Button type="primary">主要按钮</Button>
+    <ea-button type="primary">主要按钮</ea-button>
   </div>
 </template>
 <script lang="ts" setup>
-import { Button } from "@easyest/components";
+
 </script>
