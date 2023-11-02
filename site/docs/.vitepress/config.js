dad8daf0e7a070f5a46eeac3473ca4af1dc328d4
hokman <435999329@qq.com>
2023-11-02 21:20:26 +0800

    update

--- site/docs/.vitepress/config.js
+++ site/docs/.vitepress/config.js
@@ -5,7 +5,7 @@ export default {
     nav: [
       { text: '文档', link: '/guild/introduce' },
       { text: "组件", link: "/components/button/" },
-  ],
+    ],
     sidebar: {
       '/': [
         {
